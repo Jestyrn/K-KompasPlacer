341b2d073a8f1894cb7841522a8158f19ca33491
mowech <mrbug@DESKTOP-27CF1LD>
2025-07-16 12:16:57 +0400

    filled for tests

--- Program.cs
+++ Program.cs
@@ -1,9 +1,42 @@
-﻿
+﻿using netDxf;
+using netDxf.Blocks;
+using netDxf.Collections;
+using netDxf.Entities;
+using System.Drawing;
+using System.Reflection.Metadata;
+using TesterModule;
 
 internal class Program
 {
+    private static DxfDocument DXF;
+    private static List<TextInfo> Texts;
+    private static List<TextInfo> TextsInfo;
+    private static string Path = @"C:\Users\mrbug\OneDrive\Рабочий стол\Developers\K-Kompas\Kwork_Example.dxf";
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        StartNewTest();
+    }
+
+    private static void StartNewTest()
+    {
+        SetReady.Setup(Path); // почистит от пробелов и лишних символов в Entities.Text
+        TextsInfo = new List<TextInfo>(TextProcessor.GetCounterText(Path)); // парсинг текста (с шт)
+        Texts = new List<TextInfo>(TextProcessor.GetText(Path)); // парсинг текста
+
+        //NewTestsGPT.TryToDo(); // парсинг фигур
+
+        // Получить файл
+        // Превратить фрагменты в прмоугольники / полигоны
+
+        // Отсеить лишние элементы (EntitiesObjsect)
+
+        // Создание рамки + копирование документа
+        // Получить EntityObject + Line
+        // Собрать фигуру + Понять что это
+
+        // Добавить второй вариант фигуры (полигонизированная / упрощенная до прямоугольника)
+
+        // Начать строить / рассчитывать
     }
 }
