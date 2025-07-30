4657a0bbfd0e37cb13a60fff1f0ed7d72cb3b3ba
Jestyrn <138990508+Jestyrn@users.noreply.github.com>
2025-07-30 20:31:13 +0400

    Change main method

--- ConsoleDebugger/Program.cs
+++ ConsoleDebugger/Program.cs
@@ -1,14 +1,16 @@
 ﻿using netDxf;
+using netDxf.Collections;
+using netDxf.Entities;
 using TestModule;
 using TextInfo = TestModule.TextInfo;
 
 internal class Program
 {
-    private static DxfDocument DXF;
+    private static DxfDocument DXF = new DxfDocument();
     private static List<TextInfo> Texts;
     private static List<TextInfo> TextsInfo;
-    private static List<Detail> Details;
-    private static string Path = @"C:\Users\mrbug\OneDrive\Рабочий стол\Developers\K-Kompas\Kwork_Example.dxf";
+    private static List<Details> Details;
+    private static string Path = @"C:\Users\mrbug\OneDrive\Рабочий стол\Developers\K-Kompas\Kwork.dxf";
 
     private static void Main(string[] args)
     {
@@ -20,7 +22,39 @@ internal class Program
         //SetReady.Setup(Path); // почистит от пробелов и лишних символов в Entities.Text
         //TextsInfo = new List<TextInfo>(TextProcessor.GetCounterText(Path)); // парсинг текста (с шт)
         //Texts = new List<TextInfo>(TextProcessor.GetText(Path)); // парсинг текста
-        //Details = new List<Detail>(DetailsProcessor.GetAllDetails(Path));
+        Details = new List<Details>(DetailsProcessor.GetAllDetails(Path)); // парсинг деталей
+        Details = Details.OrderByDescending(p => p.Area) // сортировка деталей по увелечению
+            .ThenByDescending(p => p.Area)
+            .ThenBy(p => p.Entities.Count)
+            .ToList();
+
+        int cnt = 0;
+        foreach (var detail in Details)
+        {
+            Console.Write($"({cnt}: area {detail.Area}, ents {detail.Entities.Count}) - ");
+            cnt++;
+            if ( cnt % 5 == 0)
+                Console.WriteLine();  
+        }
+
+        int lenght = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            lenght += (int)Details[i].Width + 20;
+            Details[i].MoveDetail(lenght, 0);
+            
+            foreach (var item in Details[i].Entities)
+            {
+                DXF.Entities.Add((EntityObject)item.Clone());
+            }
+
+            foreach (var item in Details[i].BoundsDXF)
+            {
+                DXF.Entities.Add((EntityObject)item.Clone());
+            }
+        }
+
+        DXF.Save(Path.Replace("Kwork", "TestLook")); 
 
         // 1. Найти/Построить рамку для размещения
         // 2. Сделать проверку размещения
@@ -37,9 +71,5 @@ internal class Program
 
         NewTestsGPT ss = new NewTestsGPT();
         ss.TryToDo(@"C:\Users\mrbug\OneDrive\Рабочий стол\Developers\K-Kompas\Edited.dxf");
-
-        // Создать рамку
-
-        // Начать строить / рассчитывать
     }
 }
