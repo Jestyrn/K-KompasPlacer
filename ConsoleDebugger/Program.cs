c51594e28aeacd90c2911cef1a81709896268a9b
Jestyrn <138990508+Jestyrn@users.noreply.github.com>
2025-07-31 18:05:15 +0400

    Add new experemental functions

--- ConsoleDebugger/Program.cs
+++ ConsoleDebugger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using netDxf;
+using netDxf.Blocks;
 using netDxf.Collections;
 using netDxf.Entities;
 using TestModule;
@@ -22,37 +23,52 @@ internal class Program
         //SetReady.Setup(Path); // почистит от пробелов и лишних символов в Entities.Text
         //TextsInfo = new List<TextInfo>(TextProcessor.GetCounterText(Path)); // парсинг текста (с шт)
         //Texts = new List<TextInfo>(TextProcessor.GetText(Path)); // парсинг текста
-        Details = new List<Details>(DetailsProcessor.GetAllDetails(Path)); // парсинг деталей
-        Details = Details.OrderByDescending(p => p.Area) // сортировка деталей по увелечению
-            .ThenByDescending(p => p.Area)
-            .ThenBy(p => p.Entities.Count)
-            .ToList();
+        //Details = new List<Details>(DetailsProcessor.GetAllDetails(Path)); // парсинг деталей
+        //Details = Details.OrderByDescending(p => p.Area) // сортировка деталей по увелечению
+        //    .ThenByDescending(p => p.Area)
+        //    .ThenBy(p => p.Entities.Count)
+        //    .ToList();
 
-        int cnt = 0;
-        foreach (var detail in Details)
+        // --- //
+        var packer = new MaxRectsPacker(100, 100);
+
+        // 2. Добавляем детали (ширина, высота)
+        var parts = new List<Part>
         {
-            Console.Write($"({cnt}: area {detail.Area}, ents {detail.Entities.Count}) - ");
-            cnt++;
-            if ( cnt % 5 == 0)
-                Console.WriteLine();  
-        }
+            new Part { Width = 30, Height = 40, Id = 1 },
+            new Part { Width = 20, Height = 60, Id = 2 },
+            new Part { Width = 50, Height = 30, Id = 3 }
+        };
 
-        int lenght = 0;
-        for (int i = 0; i < 10; i++)
+        // 3. Пробуем упаковать
+        foreach (var part in parts.OrderByDescending(p => Math.Max(p.Width, p.Height)))
         {
-            lenght += (int)Details[i].Width + 20;
-            Details[i].MoveDetail(lenght, 0);
-            
-            foreach (var item in Details[i].Entities)
+            if (!packer.TryPlacePart(part))
             {
-                DXF.Entities.Add((EntityObject)item.Clone());
-            }
-
-            foreach (var item in Details[i].BoundsDXF)
-            {
-                DXF.Entities.Add((EntityObject)item.Clone());
+                Console.WriteLine($"Деталь {part.Id} не поместилась!");
             }
         }
+
+        // 4. Выводим результат
+        foreach (var placed in packer.PlacedParts)
+        {
+            Console.WriteLine(
+                $"Деталь {placed.Part.Id}: ({placed.X}, {placed.Y}), " +
+                $"Размер: {placed.Part.Width}x{placed.Part.Height}");
+        }
+        // --- //
+
+
+
+        FrameBuilder framer = new FrameBuilder(100);
+        List<Block> blocks = new List<Block> 
+        {
+            framer.Construct(100, 100)
+        };
+
+        foreach (var block in blocks)
+            foreach (var ent in block.Entities)
+                DXF.Entities.Add((EntityObject)ent.Clone());
 
         DXF.Save(Path.Replace("Kwork", "TestLook")); 
 
@@ -63,11 +79,6 @@ internal class Program
         //      в) Деталь не вышла за рамку
         // 3.1. Все в порядке - размещаем
         // 3.2. Все в порядке - создаем новую рамку
-
-        // Решено применение слующего паттерна расоположения
-        // 1. располагать в стиле Гильятина(каттинг)
-        // 2. рассмтривать оптимальность размещения при помощий "Поколений"
-        // 3. зафиксировать лучший результат поколения (количество поколений подберется в ручную, чтобы найти оптимальную скорость)
 
         NewTestsGPT ss = new NewTestsGPT();
         ss.TryToDo(@"C:\Users\mrbug\OneDrive\Рабочий стол\Developers\K-Kompas\Edited.dxf");
