3652ceaf223ee8d0886ce7c9c2a1df91f13a504a
Jestyrn <138990508+Jestyrn@users.noreply.github.com>
2025-07-31 18:06:52 +0400

    My Class for placing - right now ready steps for solution

---
+++ TestModule/FrameEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModule
+{
+    internal class FrameEngine
+    {
+        /*
+         * 
+         * 1. Вызов TryPlacePart()
+         * Попытка разместить без поворота
+         * Попытка разместить с 90 градусами
+         * (Вызов TryPlace)
+         * 
+         * 2. TryPlace()
+         * Поиск свободного пространства(цикл)
+         * Смотрим по эвристике(Best Short Side Fit)
+         * Находим лучшее совпадение
+         * Размещаем в Frame
+         * Делаем Split()
+         *
+         * 3. Split()
+         * Делим область на вертикаль
+         * Делим область на горизонталь
+         * Урезаем лишнее(поглощенные)
+         * MergeFreeSpace()
+         * 
+         * 4. MergeFreeSpace()
+         * Удаляем поглощение
+         * 
+         */
+
+        /*
+         * 
+         * 
+         * 
+         * 
+         */
+    }
+}
