d812cb942d3eded656d446c2c7cf8b07c0ee5536
Jestyrn <138990508+Jestyrn@users.noreply.github.com>
2025-08-23 05:27:40 +0400

    Update: added new tester for new solution

---
+++ Tester/Program.cs
@@ -0,0 +1,17 @@
+﻿using netDxf;
+using netDxf.Entities;
+
+public class Program
+{
+    private static DxfDocument dxf = new DxfDocument();
+    // Деталь на полигоны
+    // Полотно - проверить как было(гильотина), проверить "вычитание", добавить генетический алгоритм
+
+    public static void Main(string[] args)
+    {
+        string readPath = @"C:\Users\mrbug\OneDrive\Рабочий стол\Developers\K-Kompas\SimpleKwork.dxf";
+        dxf = DxfDocument.Load(readPath);
+
+        Console.WriteLine("Прочтено");
+    }
+}
